4ca0c89c69b20e93c91d32aefde415b8c08638bc
Jack Scott <jack.rob.scott@gmail.com>
2018-09-06 16:51:09 +0800

    move graph files into a single file to reduce clutter

--- src/common/user/user.schema.ts
+++ src/common/user/user.schema.ts
@@ -1,8 +1,46 @@
+import { gql } from 'apollo-server';
 import { makeExecutableSchema } from 'graphql-tools';
-import userTypeDefs from './user.graphql';
-import userResolvers from './user.resolvers';
+import User from './user.model';
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String
+    lastName: String
+  }
+
+  type Query {
+    users: [User]
+    user(id: String!): User
+  }
+
+  type Mutation {
+    addUser(firstName: String, lastName: String): User
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`;
+
+const resolvers: any = {
+  Query: {
+    users() {
+      return User.find({});
+    },
+    user(_: any, args: { id: string }) {
+      return User.findById(args.id);
+    },
+  },
+  Mutation: {
+    addUser(_: any, args: object) {
+      return User.create(args);
+    },
+  },
+};
 
 export default makeExecutableSchema({
-  typeDefs: userTypeDefs,
-  resolvers: userResolvers as any,
+  typeDefs,
+  resolvers,
 });
--- src/common/workspace/workspace.schema.ts
+++ src/common/workspace/workspace.schema.ts
@@ -1,8 +1,45 @@
+import { gql } from 'apollo-server';
 import { makeExecutableSchema } from 'graphql-tools';
-import workspaceTypeDefs from './workspace.graphql';
-import workspaceResolvers from './workspace.resolvers';
+import workspace from './workspace.model';
+
+const typeDefs = gql`
+  type Workspace {
+    id: ID!
+    name: String
+  }
+
+  type Query {
+    workspaces: [Workspace]
+    workspace(id: String!): Workspace
+  }
+
+  type Mutation {
+    addWorkspace(name: String): Workspace
+  }
+
+  schema {
+    query: Query
+    mutation: Mutation
+  }
+`;
+
+const resolvers: any = {
+  Query: {
+    workspaces() {
+      return workspace.find({});
+    },
+    workspace(_: any, args: { id: string }) {
+      return workspace.findById(args.id);
+    },
+  },
+  Mutation: {
+    addWorkspace(_: any, args: object) {
+      return workspace.create(args);
+    },
+  },
+};
 
 export default makeExecutableSchema({
-  typeDefs: workspaceTypeDefs,
-  resolvers: workspaceResolvers as any,
+  typeDefs,
+  resolvers,
 });
