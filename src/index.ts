aa1793945d08abe4ea344c51c364ba9b878ec1ff
Jack Scott <jack.rob.scott@gmail.com>
2018-09-06 14:41:45 +0800

    create basic server for graphql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('hello world');
+import { ApolloServer, gql } from 'apollo-server';
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
